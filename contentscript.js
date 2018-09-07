4e260fab8e41514e3d0878447fca5003c5917f91
brendena <cerebralasylum1@gmail.com>
2018-09-06 21:22:40 -0700

    test firefox extensions

---
+++ contentscript.js
@@ -0,0 +1,24 @@
+console.log("Load WebComponent");
+
+class MyElement extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+
+    // Create a shadow root
+    var shadow = this.attachShadow({mode: 'open'});
+
+    const doc = document.createElement('div');
+    doc.innerHTML = "Hello from Custom Element!";
+
+    // Add the element to the shadow root.
+    shadow.appendChild(doc);
+	  console.log("Added WebComponent");
+  }
+}
+
+// Define the new element
+customElements.define('my-element', MyElement);
+
+// Append the new element in the context of the host page.
+document.body.appendChild(document.createElement('my-element'));
