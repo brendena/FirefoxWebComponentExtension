c282946d8c8ce4d55f1e153cebab76671dba1d90
brendena <cerebralasylum1@gmail.com>
2018-09-17 18:22:00 -0400

    added some more test

--- contentscript.js
+++ contentscript.js
@@ -1,44 +1,57 @@
-console.log("Load WebComponent");
+//modify a tag by adding by appending element 
+
+var modifyElement =  function(parentDiv){
+  var doc = document.createElement('div');
+  doc.innerHTML = "Hello from Custom Element!";
+
+
+  var input = document.createElement('input');
+  var button = document.createElement('button');
+  button.innerHTML = "press me"
+
+  button.addEventListener("click", function(event){
+    console.log("Got the event click");
+  });
+
+
+  var image = document.createElement('img');
+  image.src = "https://d9np3dj86nsu2.cloudfront.net/image/36125d06520a2f6acdae39d1221e5ca8"
+  image.width = "100";
+  image.height = "100";
+
+  parentDiv.appendChild(doc);
+  parentDiv.appendChild(image);
+  parentDiv.appendChild(input);
+  parentDiv.appendChild(button);
+
+
+  return parentDiv;
+}
 
 class MyElement extends HTMLElement {
   constructor() {
     // Always call super first in constructor
     super();
-    // Create a shadow root
+    // Create a shadow root and a slot
     var shadow = this.attachShadow({mode: 'open'});
-
-    var doc = document.createElement('div');
-    doc.innerHTML = "Hello from Custom Element!";
-
-    var input = document.createElement('input');
-    var button = document.createElement('button');
-
-
-
-    var image = document.createElement('img');
-    image.src = "https://d9np3dj86nsu2.cloudfront.net/image/36125d06520a2f6acdae39d1221e5ca8"
-    image.width = "100";
-    image.height = "100";
-
-
-    // Add the element to the shadow root.
-    shadow.appendChild(doc);
-    shadow.appendChild(image);
-    //shadow.appendChild(input);
-    //shadow.appendChild(button);
-    console.log("Added WebComponent");
+    var slot = document.createElement('slot');
+    shadow.appendChild(slot);
+    
+    //// Add the element to the shadow root.
+    modifyElement(shadow)
+    ////try and put it into a slot
+    //modifyElement(slot)
     
 
   }
 }
 
-console.log("testing this out")
 // Define the new element
 customElements.define('my-element', MyElement);
 
-
 var myElement = document.createElement('my-element');
 
+//// force the object to be on the top of the screen
 myElement.style.position = "fixed";
 myElement.style.zIndex = "200000";
 myElement.style.width = "400px";
@@ -46,5 +59,19 @@ myElement.style.height = "100px";
 myElement.style.top = "0px";
 myElement.style.background = "white";
 
-// Append the new element in the context of the host page.
+// Append the Custom element to the host page.
 document.body.appendChild(myElement);
+
+
+/*
+//Test the same object not in a web component.
+myElement = document.createElement('div');
+myElement.style.position = "fixed";
+myElement.style.zIndex = "200000";
+myElement.style.width = "400px";
+myElement.style.height = "100px";
+myElement.style.top = "0px";
+myElement.style.background = "white";
+
+document.body.appendChild(modifyElement(myElement));
+ //*/
