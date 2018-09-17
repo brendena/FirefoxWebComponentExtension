9b0960acc736d212890d8329a1d7f6e748b4c353
Brenden Adamczak <cerebralasylum1@gmail.com>
2018-09-16 21:18:28 -0400

    added code to test webcomponents

--- contentscript.js
+++ contentscript.js
@@ -4,21 +4,47 @@ class MyElement extends HTMLElement {
   constructor() {
     // Always call super first in constructor
     super();
-
     // Create a shadow root
     var shadow = this.attachShadow({mode: 'open'});
 
-    const doc = document.createElement('div');
+    var doc = document.createElement('div');
     doc.innerHTML = "Hello from Custom Element!";
+
+    var input = document.createElement('input');
+    var button = document.createElement('button');
+
+
+
+    var image = document.createElement('img');
+    image.src = "https://d9np3dj86nsu2.cloudfront.net/image/36125d06520a2f6acdae39d1221e5ca8"
+    image.width = "100";
+    image.height = "100";
+
 
     // Add the element to the shadow root.
     shadow.appendChild(doc);
-	  console.log("Added WebComponent");
+    shadow.appendChild(image);
+    //shadow.appendChild(input);
+    //shadow.appendChild(button);
+    console.log("Added WebComponent");
+    
+
   }
 }
 
+console.log("testing this out")
 // Define the new element
 customElements.define('my-element', MyElement);
 
+
+var myElement = document.createElement('my-element');
+
+myElement.style.position = "fixed";
+myElement.style.zIndex = "200000";
+myElement.style.width = "400px";
+myElement.style.height = "100px";
+myElement.style.top = "0px";
+myElement.style.background = "white";
+
 // Append the new element in the context of the host page.
-document.body.appendChild(document.createElement('my-element'));
+document.body.appendChild(myElement);
